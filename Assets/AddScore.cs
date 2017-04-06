9aff55db038e66129efdb0537dd8bb689aa073cb
佐原慎也 <yeah.boboboi@gmail.com>
2017-04-06 21:32:20 +0900

    Lesson5 課題 得点を表示しよう

---
+++ Assets/AddScore.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddScore : MonoBehaviour {
+
+	private int score;
+
+	// Use this for initialization
+	void Start () {
+		if (tag == "SmallStarTag") {
+			score = 10;
+		} else if (tag == "LargeStarTag") {
+			score = 20;
+		} else if (tag == "SmallCloudTag") {
+			score = 30;
+		} else if (tag == "LargeCloudTag") {
+			score = 40;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//衝突時に呼ばれる関数
+	void OnCollisionEnter(Collision other) {
+		ScoreManager.score += this.score;
+	}
+}
